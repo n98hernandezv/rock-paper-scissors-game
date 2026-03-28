d3d44c5f5749310aa5503723afc89ccf00d4f82a
nicodez <n98hernandezv@gmail.com>
2026-03-28 17:13:49 -0500

    We started the score for every player. Create the playRound() function which is completed for now. Set values for every player selection and call the function to show every player's selection, say if there is a winner or it's a draw and incremente the score

--- game.js
+++ game.js
@@ -17,3 +17,46 @@ function getComputerChoice() { // Function returns "Rock", "Paper", "Scissors"
 function getHumanChoice() { // Function returns user's choice
     return prompt("¿Rock, paper or scissors?");
 }
+
+let humanScore = 0; 
+let computerScore = 0; 
+
+function playRound(computerChoice, humanChoice) {
+    computerChoice = computerChoice.toLowerCase();
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose! Paper beats Rock");
+        computerScore++;
+    }
+    else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win! Rock beats Scissors");
+        humanScore++;
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You win! Paper beats Rock");
+        humanScore++;
+    }
+    else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lose! Scissors beats Paper");
+        computerScore++;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lose! Rock beats Scissors");
+        computerScore++;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win! Scissors beats Paper");
+        humanScore++;
+    }
+    else {
+        console.log("It's a draw!");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log("Human selection: " + humanSelection);
+console.log("Computer selection: " + computerSelection);
+playRound(computerSelection, humanSelection);
